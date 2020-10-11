c77646e6beba948db50cb4a6f8893bbd3600a922
jonellwood <62469422+jonellwood@users.noreply.github.com>
2020-10-11 17:20:36 -0400

    half way through single pizza page

--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,8 +1,16 @@
 import { graphql } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
+import PizzaList from '../components/PizzaList';
 
-export default function SinglePizzaPage() {
-  return <p>Singed Pizza</p>;
+export default function SinglePizzaPage({ data: { pizza } }) {
+  return (
+    <>
+      <h2>{pizza.name}</h2>
+      <Img fluid={pizza.image.asset.fluid} />
+      <p>{pizza.toppings.name}</p>
+    </>
+  );
 }
 
 export const query = graphql`
@@ -10,6 +18,18 @@ export const query = graphql`
     pizza: sanityPizza(slug: { current: { eq: $slug } }) {
       name
       id
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      toppings {
+        name
+        id
+        vegetarian
+      }
     }
   }
 `;
