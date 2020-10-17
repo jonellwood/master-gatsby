a6cafc9c04fbae2cab38a9d31da54b01257e1ab5
Jon Ellwood <jonellwood@gmail.com>
2020-10-17 09:52:00 -0400

    slicemasters page working

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,9 +1,63 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function SlicemastersPage() {
+const SlicemasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SlicemasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+`;
+
+export default function SlicemastersPage({ data }) {
+  const slicemasters = data.slicemasters.nodes;
   return (
     <>
-      <p>Hey! I am the Slicemasters page</p>
+      <SlicemasterGrid>
+        {slicemasters.map((person) => (
+          <SlicemasterStyles>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description}</p>
+          </SlicemasterStyles>
+        ))}
+      </SlicemasterGrid>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
