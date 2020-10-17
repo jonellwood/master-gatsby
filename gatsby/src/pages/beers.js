964882e73afa25b37e02593028af52eee8270c8a
Jon Ellwood <jonellwood@gmail.com>
2020-10-17 09:16:51 -0400

    created beers page and components

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import BeerList from '../components/BeerList';
 
-export default function BeersPage() {
+export default function BeersPage({ data }) {
+  const beers = data.beers.nodes;
   return (
     <>
-      <p>Hey! I am the Beers page</p>
+      <h2 className="center">
+        We have {data.beers.nodes.length} Beers available Dine in Only!
+      </h2>
+      <BeerList beers={beers} />
     </>
   );
 }
+
+export const query = graphql`
+  query BeerQuery {
+    beers: allBeer {
+      nodes {
+        name
+        id
+        price
+        rating {
+          reviews
+          average
+        }
+        image
+      }
+    }
+  }
+`;
