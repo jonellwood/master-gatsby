07944439f34075e79aea33c1e202c07ce0732bf2
Jon Ellwood <jonellwood@gmail.com>
2020-10-18 09:31:35 -0400

    pagination completed and styling completed

--- gatsby/src/components/Pagination.js
+++ gatsby/src/components/Pagination.js
@@ -1,5 +1,31 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
+
+const PaginationStyles = styled.div`
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-items: center;
+  text-align: center;
+  border: 1px solid var(--grey);
+  margin: 2rem 0;
+  border-radius: 5px;
+  & > * {
+    padding: 1rem;
+    flex: 1;
+    border-right: 1px solid var(--grey);
+    text-decoration: none;
+    &[aria-current],
+    &.current {
+      color: var(--red);
+    }
+    &[disabled] {
+      pointer-events: none;
+      color: var(--grey);
+    }
+  }
+`;
 
 export default function Pagination({
   pageSize,
@@ -9,14 +35,26 @@ export default function Pagination({
   base,
 }) {
   const totalPages = Math.ceil(totalCount / pageSize);
-  const prePage = currentPage - 1;
+  const prevPage = currentPage - 1;
   const nextPage = currentPage + 1;
   const hasNextPage = nextPage <= totalPages;
   const hasPrevPage = prevPage >= 1;
   return (
-    <div>
-      <Link to={`${base}/${prePage}`}>&#8592; Prev</Link>
-      <Link to={`${base}/${nextPage}`}>Next &#8594; </Link>
-    </div>
+    <PaginationStyles>
+      <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
+        &#8592; Prev
+      </Link>
+      {Array.from({ length: totalPages }).map((_, i) => (
+        <Link
+          className={currentPage === 1 && i === 0 ? 'current' : ''}
+          to={`${base}/${i > 0 ? i + 1 : ''}`}
+        >
+          {i + 1}
+        </Link>
+      ))}
+      <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
+        Next &#8594;{' '}
+      </Link>
+    </PaginationStyles>
   );
 }
